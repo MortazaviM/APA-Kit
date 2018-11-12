91ac10e06492ec5d717171d0d78d22ae2e380a5c
MortazaviM <micronomicer@gmail.com>
2018-11-12 23:21:03 +0330

    some changes

--- module/enum.py
+++ module/enum.py
@@ -7,7 +7,6 @@ class Enumeration():
     def ip_finder(self,url):
         ip=socket.gethostbyname(url)
         return ip
-
     @classmethod
     def header_finder(self, url):
         response = requests.get('http://' + url)
